574bc2fa1e5e08df8ce79c8ab66395b6b2ca14b4
sazid526 <arefin15-14526@diu.edu.bd>
2023-08-12 23:34:16 +0600

    new

---
+++ app/src/main/java/com/example/myupozilla/doctor.java
@@ -0,0 +1,14 @@
+ package com.example.myupozilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctor);
+    }
+}
