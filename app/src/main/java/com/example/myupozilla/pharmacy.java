e420d0074e220ce5bfc8559abcd443c100666150
sazid526 <arefin15-14526@diu.edu.bd>
2023-08-13 11:05:30 +0600

    now

---
+++ app/src/main/java/com/example/myupozilla/pharmacy.java
@@ -0,0 +1,14 @@
+package com.example.myupozilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pharmacy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pharmacy);
+    }
+}
